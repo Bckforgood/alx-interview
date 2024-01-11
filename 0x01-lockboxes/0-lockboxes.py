ce276ebf5602e93fbe2408c3873025ae05ab2a77
Bckforgood <t.abdo.khbaza@gmail.com>
2024-01-11 19:37:01 +0000

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Lockboxes module
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+
+    Args:
+        boxes (list): A list of lists where each list represents a box and
+                      contains keys to other boxes.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+    if not boxes or not isinstance(boxes, list):
+        return False
+
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True  # The first box is always unlocked
+
+    keys_stack = [0]  # Start with the keys in the first box
+
+    while keys_stack:
+        current_box = keys_stack.pop()
+
+        for key in boxes[current_box]:
+            if 0 <= key < n and not unlocked[key]:
+                unlocked[key] = True
+                keys_stack.append(key)
+
+    return all(unlocked)
